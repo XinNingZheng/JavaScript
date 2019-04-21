9fcecf5c00768094a3db8d1e31297bb4c4809e67
XinNingZheng <827740725@qq.com>
2019-04-21 10:26:53 +0800

    views文件下的页面

--- vue/myself/src/views/Home.vue
+++ vue/myself/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="我的第一个Vue工程"/>
   </div>
 </template>
 
