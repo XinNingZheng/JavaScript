52503b9e4a70fc709be7a083596f0b091c14ea13
XinNingZheng <827740725@qq.com>
2019-05-11 11:24:26 +0800

    vue模块

---
+++ vueappzxn/src/components/Slide.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <transition-group name="." tag="ul" class="slide-box">
+            <li class="slide-item" v-for="(img,index) in imgs" :key="index" v-show="index">
+                <img :src="img"/>
+            </li>  
+        </transition-group>
+    </div>    
+</template>
+
+<script>
+    export default {
+        props:['imgs'],
+        data() {
+            return {
+                idx:0
+            }
+        },
+
+    }
+</script>
+
+<style scope>
+    .slide-box{
+        position: relative;
+    }
+    .slide-item{
+        position: absolute;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 4rem;
+    }
+    /* .slide-item:first-child{
+        z-index: 2;
+    } */
+    /* .slide-item:first-child{
+        display: block;
+    } */
+    .slide-item img{
+        width: 100%;
+        height: 100%;
+    }
+
+
+</style>
--- vueappzxn/src/views/book.vue
+++ vueappzxn/src/views/book.vue
@@ -1,15 +1,86 @@
 <template>
     <div>
-图书
+        <!-- <swipe class="my-swipe">
+            <swipe-item class="slide1"></swipe-item>
+            <swipe-item class="slide2"></swipe-item>
+            <swipe-item class="slide3"></swipe-item>
+        </swipe> -->
+
+        <slide :imgs="imgs"></slide>
     </div>
 </template>
-
 <script>
-    export default {
-        
+    // require('vue-swipe/dist/vue-swipe.css');
+    // import { Swipe, SwipeItem } from 'vue-swipe';
+    // export default {
+    //     components: {
+    //         Swipe,
+    //         SwipeItem
+    //     }
+    // }
+    
+    //自定义的轮播图组件
+    import Slide from '@/components/Slide.vue'
+    export default{
+        data() {
+            return {
+                imgs:[
+                    "https://img3.doubanio.com/lpic/s24468373.jpg",
+                    "https://img3.doubanio.com/lpic/s27102925.jpg",
+                    "https://img3.doubanio.com/lpic/s6989253.jpg"
+                ]
+            }
+        },
+        components:{
+            Slide
+        }
     }
+
 </script>
 
 <style scoped>
+/* .my-swipe {
+  height: 200px;
+  color: #fff;
+  font-size: 30px;
+  text-align: center;
+}
 
+.slide1 {
+  background-color: green;
+  color: #fff;
+
+}
+
+
+.slide2 {
+  background-color: #ffd705;
+  color: #000;
+
+}
+
+.slide3 {
+  background-color: #ff2d4b;
+  color: #fff;
+
+} */
+/* 过度的动画  */
+    .fade-enter{
+        transform: translateX(-100%);
+    }
+    .fade-enter-active{
+        transition:transform 1s;
+    }
+    .fade-enter-to{
+        transform: translateX(0);
+    }
+    .fade-leave{
+        transform: translateX(0);
+    }
+    .fade-leave-active{
+        transition:transform 1s;
+    }
+    .fade-leave-to{
+        transform: translateX(100%);
+    }
 </style>
